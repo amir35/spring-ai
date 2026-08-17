24f33982c9e4846a58e3f03656485a38797b4f40
MOHD AMIR AHMAD <amir.engineer786@gmail.com>
2026-08-17 08:34:53 +0300

    Implementing Rag with vector store and embedding

---
+++ src/main/java/com/amir35/spring_ai/service/EmbeddingService.java
@@ -0,0 +1,37 @@
+package com.amir35.spring_ai.service;
+
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmbeddingService {
+
+    private final EmbeddingModel embeddingModel;
+
+    public EmbeddingService(EmbeddingModel embeddingModel) {
+        this.embeddingModel = embeddingModel;
+    }
+
+    public float[] generateEmbedding(String text) {
+        return embeddingModel.embed(text);
+    }
+
+    public double cosineSimilarity(float[] vectorA, float[] vectorB) {
+
+        double dotProduct = 0.0;
+        double magnitudeA = 0.0;
+        double magnitudeB = 0.0;
+
+        for (int i = 0; i < vectorA.length; i++) {
+
+            dotProduct += vectorA[i] * vectorB[i];
+
+            magnitudeA += vectorA[i] * vectorA[i];
+
+            magnitudeB += vectorB[i] * vectorB[i];
+        }
+
+        return dotProduct /
+                (Math.sqrt(magnitudeA) * Math.sqrt(magnitudeB));
+    }
+}
