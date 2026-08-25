1f7e0c0ef60060eb7f96b7a185d2c5b957e252e0
MOHD AMIR AHMAD <amir.engineer786@gmail.com>
2026-08-25 08:41:05 +0300

    Organize backend and frontend projects

--- src/main/java/com/amir35/spring_ai/dto/ChatRequest.java
+++
@@ -1,4 +0,0 @@
-package com.amir35.spring_ai.dto;
-
-public record ChatRequest(String message) {
-}
--- src/main/java/com/amir35/spring_ai/dto/ChatResponse.java
+++
@@ -1,4 +0,0 @@
-package com.amir35.spring_ai.dto;
-
-public record ChatResponse(String answer) {
-}
